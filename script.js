dc43df63a688ce313b2b250ad80b45b48c5a4abc
Derek Stapleton <derek.stapleton21@gmail.com>
2020-03-21 11:18:16 -0400

    javascript things again

---
+++ script.js
@@ -0,0 +1,32 @@
+var likesToTravel = true;
+
+if (likesToTravel === true){
+
+    console.log("Bon Voyage")
+}else
+ {console.log("Stay on the couch!!")}
+
+ var age = 17
+ if(age >= 18){console.log("go vote")}
+ else {
+     console.log("you cant't vote")
+ }
+
+ var isWereWolf = true;
+ var isFullMoon = true;
+
+ if(isWereWolf === true&& isFullMoon == true){
+     console.log("Run, imma turn into a Were Wolf.")
+ }else if (isWereWolf === true && isFullMoon == false){
+     console.log("im still a werewolf, but we are good right now.")
+ }else if (isWereWolf === false && isFullMoon == true){
+     console.log("its a beautiful full moon, and im glad im not a were wolf.")
+ }
+
+ var favoriteFood = "waffles"
+ if (favoriteFood === "fried pickles" && favoriteFood ==="pepperoni rolls"){
+     console.log("Congrats, you have excellent taste!")
+ }
+ else (favoriteFood === "fried pickles" && favoriteFood ==="pepperoni rolls")
+ console.log("clearly, you have not tried fried pickles or pepperoni rolls.")
+ 
